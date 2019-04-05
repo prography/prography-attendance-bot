d429ae296e09212d524acf24d34c8871e57b2355
Sung Man Cho <pccho9108@gmail.com>
2019-04-06 00:15:02 +0900

    slackbot

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const {RTMClient} = require('@slack/client');
+const token = process.env.SLACK_TOKEN || 'xoxb-498008364722-602282977333-Qf1K8EonzKmHdNDATPkSCgmr';
+const rtm = new RTMClient(token);
+rtm.start();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+rtm.on('message',(message)=>{
+	var text = message.text
+
+	if(text.includes("납부")){
+		rtm.sendMessage("성실한 납부 감사드립니다 :)", message.channel);
+	}
+});
