f6b601b292539165dade16eee8c1937931e56488
Sung Man Cho <pccho9108@gmail.com>
2019-12-08 16:25:29 +0900

    user test

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const token = process.env.SLACK_TOKEN || 'xoxb-259311417568-788589402980-TTySjkE
 const rtm = new RTMClient(token);
 rtm.start();
 
-rtm.on('message',(message)=>{
-	
-	var text = message.text
-	var user = message.user
-	
-	if(text.includes("뒷풀이")){
-		rtm.sendMessage("<@${message.user}> 뒷풀이 불참 확인 완료하였습니다.", message.channel);
-	}
+rtm.on('message',async (event) => {
+	try {
+		if event.message.includes("뒷풀이")){
+			rtm.sendMessage("뒷풀이 불참 확인 완료하였습니다.", event.message.channel);
+		}
+		
+		if (event.message.includes("세션") && !event.message.includes("특수")){
+			rtm.sendMessage("세션 불참 확인 완료하였습니다. (일반결석)", event.message.channel);
+		}
+		
+		else if (event.message.includes("세션") && event.message.includes("특수")){
+			rtm.sendMessage("세션 불참 확인 완료하였습니다. (특수결석)", event.message.channel);
+		}
 
-	if(text.includes("세션") && text.includes("특수")){
-		rtm.sendMessage("세션 불참 확인 완료하였습니다 (특수결석).", message.channel);
-	}
-
-	else if(text.includes("세션") && !text.includes("특수")){
-		rtm.sendMessage("세션 불참 확인 완료하였습니다 (일반결석).", message.channel);
+	}catch (error){
+		console.log('error', error)
 	}
 });
