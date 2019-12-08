75a8d2dd23a6b15581b982d38967a96b0853e6ed
Sung Man Cho <pccho9108@gmail.com>
2019-12-08 15:39:59 +0900

    메시지 수신 조건문 수정

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const rtm = new RTMClient(token);
 rtm.start();
 
 rtm.on('message',(message)=>{
+	
 	var text = message.text
-
-	if(text.includes("뒷풀이")){
+	
+	if(text.includes("뒷풀이"))
+	{
 		rtm.sendMessage("뒷풀이 불참 확인 완료하였습니다.", message.channel);
 	}
-	
-	if(text.includes("세션")){
-		rtm.sendMessage("세션 불참 확인 완료하였습니다 (일반결석).", message.channel);
+
+	if(text.includes("세션") && text.includes("특수"))
+	{
+		rtm.sendMessage("세션 불참 확인 완료하였습니다 (특수결석).", message.channel);
 	}
 
-	if(text.includes("세션") && text.includes("특수")){
-		rtm.sendMessage("세션 불참 확인 완료하였습니다 (특수결석).", message.channel);
+	else if(text.includes("세션"))
+	{
+		rtm.sendMessage("세션 불참 확인 완료하였습니다 (일반결석).", message.channel);
 	}
 });
