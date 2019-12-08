011246bfffcc9e1568188da53ec16764ada3e40e
Sung Man Cho <pccho9108@gmail.com>
2019-12-08 15:23:26 +0900

    slack bot update (attendance check)

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const {RTMClient} = require('@slack/client');
-const token = process.env.SLACK_TOKEN || 'xoxb-498008364722-602282977333-Qf1K8EonzKmHdNDATPkSCgmr';
+const token = process.env.SLACK_TOKEN || 'xoxb-259311417568-788589402980-TTySjkE9YyNeRh5G2nBvvTjO';
+
 const rtm = new RTMClient(token);
 rtm.start();
 
 rtm.on('message',(message)=>{
 	var text = message.text
 
-	if(text.includes("납부")){
-		rtm.sendMessage("성실한 납부 감사드립니다 :)", message.channel);
+	if(text.includes("뒷풀이")){
+		rtm.sendMessage("뒷풀이 불참 확인 완료하였습니다.", message.channel);
+	}
+	
+	if(text.includes("세션")){
+		rtm.sendMessage("세션 불참 확인 완료하였습니다 (일반결석).", message.channel);
+	}
+
+	if(text.includes("세션") && text.includes("특수")){
+		rtm.sendMessage("세션 불참 확인 완료하였습니다 (특수결석).", message.channel);
 	}
 });
